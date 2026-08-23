0559af6dd5064245af98ef0b298b1fcfe7e62cc2
Shivansh <shivansh.rbj@gmail.com>
2026-08-23 22:08:14 +0530

    Added 2a

---
+++ 2a.cpp
@@ -0,0 +1,24 @@
+//Function 
+
+#include<iostream>
+using namespace std;
+
+int sum (int a, int b){
+    int c = a+b;
+    return c;
+}
+
+int main (){
+    int num1 , num2;
+
+    cout <<"Enter first no. : ";
+    cin >>num1;
+
+    cout <<"Enter second no. :";
+    cin >>num2;
+
+    cout <<"The sum is : "<<sum(num1,num2);
+
+    return 0;
+
+}
