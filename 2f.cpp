6809eb8bfeeaf23dc9b5de5f2d23f3051c9a351a
Shivansh <shivansh.rbj@gmail.com>
2026-08-31 10:45:47 +0530

    Added 2f

---
+++ 2f.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+inline int product(int a, int b){
+    return a *b;
+}
+
+int main(){
+    int x = 2;
+    int y = 3;
+
+    cout<<"The product of x & y is "<<product(x,y) <<endl;
+
+    return 0;
+}
