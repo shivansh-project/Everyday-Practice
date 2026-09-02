d203f181eadf8fd68a3c919204a06aba75e00f80
Shivansh <shivansh.rbj@gmail.com>
2026-09-02 12:38:11 +0530

    Added Vector 1a

---
+++ Vector/1a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+// Vector is a dynamic array.
+int main(){
+
+    vector<int> v;
+
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
+
+
+    cout <<v.at(0) <<endl;
+    cout <<v.at(1) <<endl;
+    cout <<v.at(2) <<endl;
+    return 0;
+}
