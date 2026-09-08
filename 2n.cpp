f380c28eb4e43b4d4913d8ddf82ff6f5e537f295
Shivansh <shivansh.rbj@gmail.com>
2026-09-08 23:39:19 +0530

    Removed some files

--- 2n.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main(){
-    cout <<"Nothing to output";
-
-    return 0;
-}
--- 2o.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-    cout <<"Aptitude Practice.";
-    return 0;
-}
